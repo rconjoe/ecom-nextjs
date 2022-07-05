8ed59e8867c2c7b01f1f346fc1bf9f84db04b784
rconjoe <joe@rconjoe.com>
2022-07-05 15:45:20 -0700

    fix slug?

--- components/SingleProduct.js
+++ components/SingleProduct.js
@@ -1,28 +1,33 @@
 import styles from "./SingleProduct.module.css";
 import Image from "next/image";
 
-export default function SingleProduct(props) {
-  console.log(props)
-  const { id, imageUrl, name, url, description, price } = props
+export default function SingleProduct({
+  item_id,
+  title,
+  slug,
+  images,
+  description,
+  price
+}) {
 
   return (
-    <div key={id} className={styles.product}>
+    <div key={item_id} className={styles.product}>
       <Image
-        src={imageUrl}
-        alt={`Image of ${name}`}
+        src={'http://placekitten.com/640/640'}
+        alt={`Image of ${title}`}
         height={640}
         width={640}
       />
-      <h3>{name}</h3>
+      <h3>{title}</h3>
       <p>{description}</p>
       <span>${price}</span>
       <div>
         <button
           className="snipcart-add-item"
-          data-item-id={id}
-          data-item-image={imageUrl}
-          data-item-name={name}
-          data-item-url={url}
+          data-item-id={item_id}
+          data-item-image='http://placekitten.com/640/640'
+          data-item-name={title}
+          data-item-url={`${window.location.hostname}/${slug}`}
           data-item-price={price}
           data-item-description={description}
         >
